f5e8e037f1c00d1e0afcd6c0ec3f8754f4f5f731
Juan Sebastian Charria <163075911+JuanSebastianCharria@users.noreply.github.com>
2024-08-12 19:45:16 -0500

    Agregando archivos desde el repositorio original

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+   // En Mayúsculas y singular      // en minúsculas y singular
+const User = sequelize.define('user', {
+    // Definimos las columnas aquí
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    birthday: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    }
+});
+module.exports = User;
+
+
+
+
+
+
+
+
+
